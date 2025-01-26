e93c94bc1651dd01a829ac33ea4e964939b11de1
Anton Nordbeck <anton.nordbeck@gmail.com>
2025-01-26 19:27:32 +0100

    First baseline implementation, comparing all pairs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Pos{
+    x: f32,
+    y: f32,
+    z: f32
+}
+
+fn main(){
+    let mut rdr = csv::ReaderBuilder::new().has_headers(false).delimiter(b' ').from_path("positions.xyz").unwrap();
+    let positions: Vec<Pos> = rdr.deserialize().map(|pos| pos.unwrap()).collect();
+
+    let mut num = 0;
+    for i in 0..positions.len(){
+        for j in (i+1)..positions.len(){
+            let dist = f32::sqrt((positions[i].x+positions[j].x).powi(2)+(positions[i].y+positions[j].y).powi(2)+(positions[i].z+positions[j].z).powi(2));
+            if dist <= 0.05{
+                num += 1;
+            }
+        }
+    }
+    println!("{}", num)
 }
